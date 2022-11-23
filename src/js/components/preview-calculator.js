97ef8910213e8923ca352cfdb2bc52de6b913a6c
Nik100500 <nikita.nikitasenko@gmail.com>
2022-11-23 13:22:58 +0300

    add slider, start work with slide animation

--- src/js/components/preview-calculator.js
+++ src/js/components/preview-calculator.js
@@ -1,5 +1,5 @@
 var exporterCheckbox = document.getElementById('exporter');
 var exporterInput = document.getElementById('exporter-input');
-exporterCheckbox.addEventListener('click', function () {
+exporterCheckbox !== null ? exporterCheckbox.addEventListener('click', function () {
   exporterCheckbox.checked ? exporterInput.classList.add('active') : exporterInput.classList.remove('active')
-})
+}) : ''
