9048158848a5ba0d01e35c4596e9d4dcb3a2dd2e
Nik100500 <nikita.nikitasenko@gmail.com>
2022-12-03 02:26:44 +0300

    add orsk map

--- src/js/components/select-item-map.js
+++ src/js/components/select-item-map.js
@@ -1,7 +1,9 @@
 var itemsMap = document.querySelectorAll('.my-img__point')
 var itemsList = document.querySelectorAll('.item__point')
 var orenburgItems = document.querySelector('.content-orenburg__items');
-function activeItem(itemActive) {
+var orskItems = document.querySelector('.content-orsk__items');
+function activeItem(itemActive, mapItems) {
+  console.log(mapItems)
   itemsMap.forEach(item => {
     item.classList.remove('active')
     item.dataset.pointFilter == itemActive.dataset.pointFilter ? item.classList.add('active') : ''
@@ -10,14 +12,14 @@ function activeItem(itemActive) {
   itemsList.forEach(item => {
     item.classList.remove('active')
     item.dataset.pointFilter == itemActive.dataset.pointFilter ? 
-    (item.classList.add('active'), orenburgItems.className.indexOf('hidden') == -1 ? item.scrollIntoView({ behavior: 'smooth'}) : '') 
+    (item.classList.add('active'), mapItems.className.indexOf('hidden') == -1 ? item.scrollIntoView({ behavior: 'smooth'}) : '') 
     : ''
   })
   itemActive.classList.add('active')
 }
-itemsMap.forEach(item => {
-  item.addEventListener('click', function () { activeItem(item); })
-})
-itemsList.forEach(item => {
-  item.addEventListener('click', function () { activeItem(item); })
-})
+itemsMap !== null ? itemsMap.forEach(item => {
+  item.addEventListener('click', function () { activeItem(item, orenburgItems !== null ? orenburgItems : orskItems !== null ? orskItems : ""); })
+}) : ""
+itemsList !== null ? itemsList.forEach(item => {
+  item.addEventListener('click', function () { activeItem(item, orenburgItems !== null ? orenburgItems : orskItems !== null ? orskItems : ""); })
+}) : ""
--- src/js/components/sidebar-map.js
+++ src/js/components/sidebar-map.js
@@ -1,5 +1,6 @@
                 
   var orenburgItems = document.querySelector('.content-orenburg__items');
+  var orskItems = document.querySelector('.content-orsk__items');
   if (orenburgItems !== null) {
     var orenburgItemsButton = document.querySelector('.content-orenburg__items-button');
     var orenburgItemsOverlay = document.querySelector('.content-orenburg__items-overlay');
@@ -12,4 +13,17 @@
     }
     orenburgItemsButton.addEventListener('click', hideOrenburgItems)
     orenburgItemsOverlay.addEventListener('click', hideOrenburgItems)
+  } else if (orskItems !== null) {
+    var orskItemsButton = document.querySelector('.content-orsk__items-button');
+    var orskItemsOverlay = document.querySelector('.content-orsk__items-overlay');
+    function hideOrskItems() {
+      orskItems.classList.toggle('hidden');
+      orskItemsOverlay.classList.toggle('hidden');
+      setTimeout(() => {
+        orskItemsOverlay.classList.toggle('animation-hidden');
+      }, 250);
+    }
+    orskItemsButton.addEventListener('click', hideOrskItems)
+    orskItemsOverlay.addEventListener('click', hideOrskItems)
+
   }
